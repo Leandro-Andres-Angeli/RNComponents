0cf08f56c644eebd90752b49cd0e44709dbe707e
Leandro Andres Angeli <leandroandresangeli@gmail.com>
2025-08-08 23:35:47 -0300

    finished theming

--- src/components/HeaderTitle.tsx
+++ src/components/HeaderTitle.tsx
@@ -1,14 +1,18 @@
 /* eslint-disable react-native/no-inline-styles */
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useContext } from 'react';
 import { styles } from '../theme/AppTheme';
+import { ThemeConext } from '../context/themeContext/ThemeContext';
 interface Props {
   title: string;
 }
 const HeaderTitle = ({ title }: Props) => {
+  const {
+    theme: { colors },
+  } = useContext(ThemeConext);
   return (
     <View style={{ marginVertical: 20, paddingHorizontal: 20 }}>
-      <Text style={styles.title}>{title}</Text>
+      <Text style={{ ...styles.title, color: colors.text }}>{title}</Text>
     </View>
   );
 };
